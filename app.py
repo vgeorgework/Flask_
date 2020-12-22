907805ad5afee667531c19fd9df741e930f9a9dc
vineeth <vgeorgework@gmail.com>
2020-12-22 11:05:53 +0530

    secrets added

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ app = Flask(__name__)
 #db = yaml.load(open('db.yaml'))
 # app.config['MYSQL_HOST'] = "mysql"
 # app.config['MYSQL_DB'] = "TEST"
-app.config['MYSQL_HOST'] = os.getenv('mysql')
+# app.config['MYSQL_USER'] = "root"
+# app.config['MYSQL_PASSWORD'] = "toor"
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "toor"
-
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 
 mysql = MySQL(app)
 
