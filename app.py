74fdcb778939c79e46d1dfa9c2838475e36372d0
Vineeth george <39851362+vgeorgework@users.noreply.github.com>
2020-12-24 12:24:35 +0530

    appfile changed

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 # app.config['MYSQL_DB'] = "TEST"
 # app.config['MYSQL_USER'] = "root"
 # app.config['MYSQL_PASSWORD'] = "toor"
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_HOST'] = "mysql"
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DATABASE')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 
