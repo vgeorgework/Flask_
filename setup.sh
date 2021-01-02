0d8456b0687a1bee06a165a6e6e959c7ad03661d
Vineeth george <39851362+vgeorgework@users.noreply.github.com>
2021-01-02 18:16:02 +0530

    slight changes

--- setup.sh
+++ setup.sh
@@ -8,15 +8,7 @@ eval $(minikube docker-env)
 helm install --set db.username=testuser,db.password=user@123 flaskapp helmcharts/
 kubectl get all
 echo "sleeper executed for around 2 mins for cluster creation............................................................."
-sleep 5
-echo " you can abort this step and have the control or you can wait for some time while we create the cluster......................................................"
-sleep 5
-echo "checking the pod status................................................................"
-kubectl get po
-sleep 5
-echo "depending upon your connection it may take less time ..............................................................."
-sleep 10
-echo "checking the pod status again ! ................................................................ "
+sleep 1m
 kubectl get po
 sleep 20
 kubectl get all
@@ -24,6 +16,6 @@ sleep 1m
 echo "checking with kubectl get all ...................................................... "
 sleep 5
 kubectl get all
-echo "executing service command in minikube ..................................................................."
+echo "executing service command in minikube 
 sleep 5
 minikube service flask-web-svc
